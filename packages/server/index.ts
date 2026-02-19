8d38c817596c354a0b973f652fe353b369b1cf31
Ikenna U <iykethe1st@gmail.com>
2026-02-19 18:11:16 +0100

    connect the FE and BE

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from the server!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "API Hello from the server!" });
+});
+
 app.listen(port, () => {
   console.log(
     `Server is running on http://localhost:${process.env.OPENAI_API_KEY}`,
