3a2e9260b619df68e12ac1e03d749ee081d5461a
Ikenna U <iykethe1st@gmail.com>
2026-02-26 18:59:33 +0100

    set up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error fetching message:", error));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message))
+         .catch((error) => console.error('Error fetching message:', error));
+   }, []);
 
-  return (
-    <div className="App">
-      <h1 className="font-bold text-red-400">{message}</h1>
-      <Button>Click me</Button>
-    </div>
-  );
+   return (
+      <div className="App">
+         <h1 className="font-bold text-red-400">{message}</h1>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
 export default App;
