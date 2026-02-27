8635e49cd639ffeec464738fa387fed82c276546
Ikenna U <iykethe1st@gmail.com>
2026-02-27 17:37:01 +0100

    managing conversation state

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
